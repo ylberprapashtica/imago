5a58b434a2420305e48de24e30ba08fc71c6c2a8
Ylber Prapashtica <ylberprapashtica@gmail.com>
2025-03-22 12:00:50 +0100

    Fix Vercel v0.9

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-# Install PHP and required extensions
-echo "Installing PHP and extensions..."
-apt-get update && apt-get install -y \
-    php8.2 \
-    php8.2-cli \
-    php8.2-common \
-    php8.2-curl \
-    php8.2-mbstring \
-    php8.2-xml \
-    php8.2-zip \
-    php8.2-pgsql \
-    php8.2-pdo-pgsql
-
-# Install Composer
-echo "Installing Composer..."
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-
-# Verify installations
-echo "Verifying installations..."
-php -v
-composer -V
-
-# Install PHP dependencies
-echo "Installing PHP dependencies..."
-composer install --no-dev --optimize-autoloader
-
 # Install Node.js dependencies
 echo "Installing Node.js dependencies..."
 npm install
